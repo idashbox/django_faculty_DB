573bbdf0a241e7fa6e13922787e27f1d2d6fbee4
idashbox <dmerkusina@gmail.com>
2024-11-05 11:57:09 +0300

    add crud

--- django_faculty_DB/urls.py
+++ django_faculty_DB/urls.py
@@ -4,8 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
-    path('users/', views.user_list, name='user_list'),
-    path('teachers/', views.teacher_list, name='teacher_list'),
-    path('departments/', views.department_list, name='department_list'),
+    path('users/', views.UserListView.as_view(), name='user_list'),
+    path('users/new/', views.UserCreateView.as_view(), name='user_new'),
+    path('users/<int:pk>/edit/', views.UserUpdateView.as_view(), name='user_edit'),
+    path('users/<int:pk>/delete/', views.UserDeleteView.as_view(), name='user_delete'),
+    path('teachers/', views.TeacherListView.as_view(), name='teacher_list'),
+    path('teachers/new/', views.TeacherCreateView.as_view(), name='teacher_new'),
+    path('teachers/<int:pk>/edit/', views.TeacherUpdateView.as_view(), name='teacher_edit'),
+    path('teachers/<int:pk>/delete/', views.TeacherDeleteView.as_view(), name='teacher_delete'),
+    path('departments/', views.DepartmentListView.as_view(), name='department_list'),
+    path('departments/new/', views.DepartmentCreateView.as_view(), name='department_new'),
+    path('departments/<int:pk>/edit/', views.DepartmentUpdateView.as_view(), name='department_edit'),
+    path('departments/<int:pk>/delete/', views.DepartmentDeleteView.as_view(), name='department_delete'),
 ]
